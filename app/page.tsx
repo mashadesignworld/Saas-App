55bcca3dcb5e3b5298338bbb11b29aa3a246cdd9
YourNewGitHubUsername <njugunak.kevin@gmail.com>
2026-03-09 09:46:01 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '../components/ui/button';
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Page</h1>
+      <Button>Click me</Button> 
+    </div>
+  )
+}
+
+export default Page
